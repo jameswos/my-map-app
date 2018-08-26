0838b46db4e4146e779711f44270f90be46b35b9
James Woszczynski <jameswoszczynski@gmail.com>
2018-08-26 12:20:05 +0100

    Centre position set to Leicester

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ class App extends Component {
 
   initMap = () => {
     let map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
+      center: { lat: 52.637106, lng: -1.139771 },
+      zoom: 14
     });
   }
 
@@ -29,13 +29,10 @@ class App extends Component {
   }
 }
 
-/*
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCdU03TvjRBVim4B5U3qa95CuwbVJN4Q2E&callback=initMap" async defer></script>
-*/
-
 export default App;
 
 function loadScript(url) {
+  // https://stackoverflow.com/questions/34779489/rendering-a-google-map-without-react-google-map
   let ref = window.document.getElementsByTagName('script')[0];
   let script = window.document.createElement('script');
 
