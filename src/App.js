3d77d8f869e0a7af876e48c6ca47953421e829d1
James Woszczynski <jameswoszczynski@gmail.com>
2018-08-26 12:14:40 +0100

    Map loaded! :)

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import './App.css';
 
 class App extends Component {
 
+  componentDidMount() {
+    this.loadMap();
+
+  }
+
+  loadMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCdU03TvjRBVim4B5U3qa95CuwbVJN4Q2E&callback=initMap");
+    window.initMap = this.initMap;
+  }
+
   initMap = () => {
-    const map = new google.maps.Map(document.getElementById('map'), {
+    let map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: -34.397, lng: 150.644},
       zoom: 8
     });
   }
-  
+
   render() {
     return (
       <div className="App">
@@ -24,3 +34,17 @@ class App extends Component {
 */
 
 export default App;
+
+function loadScript(url) {
+  let ref = window.document.getElementsByTagName('script')[0];
+  let script = window.document.createElement('script');
+
+  script.src = url;
+  script.async = true;
+  script.defer = true;
+  ref.parentNode.insertBefore(script, ref);
+
+  script.onerror = function () {
+    document.write('Load error: Google Maps')
+  };
+}
